2530839c79e57b4f2c96854acb5357f06088519d
Dương Tùng Anh <tunnaduong@gmail.com>
2019-06-25 22:53:20 +0700

    detect screen size

---
+++ thuvienanh/detect.js
@@ -0,0 +1,3 @@
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    window.location = "https://tunganh03.github.io/github-pages-gallery/index.html"; 
+}
